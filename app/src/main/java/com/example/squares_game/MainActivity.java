a31e8f99a101dea820e7efcf0c4b0ab424b7233f
Cyaira <hughesc2@vcu.edu>
2022-02-23 15:33:41 -0500

    Initial Commit

---
+++ app/src/main/java/com/example/squares_game/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.squares_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        int i = 0;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
