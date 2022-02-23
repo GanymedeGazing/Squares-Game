5ce2f84a48fb13866f66002e42569d7b310bfef3
Cyaira <hughesc2@vcu.edu>
2022-02-23 15:40:56 -0500

    I am literally trying to see how this works.

--- app/src/main/java/com/example/squares_game/MainActivity.java
+++ app/src/main/java/com/example/squares_game/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        int j = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
