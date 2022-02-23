d62bfb2c8271013650f688d8d11f1507f51ee9e4
Cyaira <hughesc2@vcu.edu>
2022-02-23 15:37:46 -0500

    Initial Commit

--- app/src/main/java/com/example/squares_game/MainActivity.java
+++ app/src/main/java/com/example/squares_game/MainActivity.java
@@ -8,7 +8,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        int i = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
